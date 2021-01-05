270f843bb49d3acfe1531b639861e3a65600289a
Jasmin Jenull <jasmin.jenull@gmx.at>
2021-01-05 18:00:07 +0100

    Get input from form and create todo object in list, render TodoList component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,37 @@
-function App() {
-  return <div></div>;
-}
+import React, { useState } from "react";
+import "./app.css";
 
-export default App;
+// components
+import TodoList from "./Components/TodoList/TodoList";
+
+export default function App() {
+  let [input, setInput] = useState("");
+  const [todoList, setTodoList] = useState([]);
+
+  const onSubmit = () => {
+    const newTodo = {
+      id: todoList.length + 1,
+      task: input,
+      isDone: false,
+    };
+    if (todoList.length === 0) {
+      setTodoList(todoList.concat(newTodo));
+    } else {
+      setTodoList([...todoList, newTodo]);
+    }
+    setInput("");
+  };
+
+  return (
+    <div className="App">
+      <div className="InputForm">
+        <input
+          value={input}
+          onInput={(event) => setInput(event.target.value)}
+        />
+        <button onClick={onSubmit}>Submit</button>
+      </div>
+      <TodoList todoList={todoList} />
+    </div>
+  );
+}
