4d893b3047d85df6f5246a84117de4eb46695937
Jasmin Jenull <jasmin.jenull@gmx.at>
2021-01-07 14:29:29 +0100

    Add drag and drop functionality

--- src/Components/Todo/Todo.js
+++ src/Components/Todo/Todo.js
@@ -2,12 +2,13 @@
 import "./todo.css";
 
 export default function Todo({ todo, todoList, setTodoList }) {
-
   const markAsDone = () => {
     const todoUpdatedArray = todoList.map((todoObject) => {
       if (todoObject.id === todo.id) {
         //todoObject.isDone = true;
-        todoObject.isDone === true ? todoObject.isDone = false : todoObject.isDone = true;
+        todoObject.isDone === true
+          ? (todoObject.isDone = false)
+          : (todoObject.isDone = true);
         //console.log(todoObject);
         return todoObject;
       } else {
@@ -17,20 +18,18 @@ export default function Todo({ todo, todoList, setTodoList }) {
     setTodoList(todoUpdatedArray);
   };
 
-  
   const deleteTask = () => {
     todoList.map((todoObject) => {
-      if(todoObject.id === todo.id) {
+      if (todoObject.id === todo.id) {
         todoList.splice(todoObject, 1);
       }
       console.log(todoList);
     });
     setTodoList([...todoList]);
-  }
+  };
 
   return (
     <div className="task">
-      {console.log(todoList)}
       <p
         className={
           todo.isDone === true ? "task-desc finished" : "task-desc to-finish"
@@ -42,9 +41,16 @@ export default function Todo({ todo, todoList, setTodoList }) {
       <button
         onClick={markAsDone}
         className={"task-button fas fa-check"}
-        className={todo.isDone === false ? "task-button fas fa-check" : "task-button fas fa-undo"}
+        className={
+          todo.isDone === false
+            ? "task-button fas fa-check"
+            : "task-button fas fa-undo"
+        }
       ></button>
-      <button onClick={deleteTask} className="task-button far fa-trash-alt"></button>
+      <button
+        onClick={deleteTask}
+        className="task-button far fa-trash-alt"
+      ></button>
     </div>
   );
 }
--- src/Components/TodoList/TodoList.js
+++ src/Components/TodoList/TodoList.js
@@ -1,3 +1,5 @@
+import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
+
 // Import components
 import Todo from "../Todo/Todo";
 
@@ -5,17 +7,59 @@ import Todo from "../Todo/Todo";
 import "./todo-list.css";
 
 export default function TodoList({ todoList, setTodoList }) {
+  const handleOnDragEnd = (result) => {
+    if (!result.destination) return;
+    const [reorderedTodos] = todoList.splice(result.source.index, 1);
+    todoList.splice(result.destination.index, 0, reorderedTodos);
+  };
+
+  const getItemStyle = (isDragging, draggableStyle) => ({
+    userSelect: "none",
+    background: isDragging ? "rgba((31,35,42, 1)" : "1f232a",
+    ...draggableStyle,
+  });
+
   return (
-    <div className="task-wrapper">
-      {todoList.map((todo) => {
-        return (
-          <Todo
-            todo={todo}
-            todoList={todoList}
-            setTodoList={(todoList) => setTodoList(todoList)}
-          />
-        );
-      })}
-    </div>
+    <DragDropContext onDragEnd={handleOnDragEnd}>
+      <Droppable droppableId="task-droppableId">
+        {(provided) => (
+          <div
+            className="droppableId outer-task-wrapper"
+            {...provided.droppableProps}
+            ref={provided.innerRef}
+          >
+            {todoList.map((todo, index) => {
+              return (
+                <Draggable
+                  key={todo.id}
+                  draggableId={todo.id.toString()}
+                  index={index}
+                >
+                  {(provided, snapshot) => (
+                    <div
+                      class="wrapper"
+                      ref={provided.innerRef}
+                      {...provided.draggableProps}
+                      {...provided.dragHandleProps}
+                      style={getItemStyle(
+                        snapshot.isDragging,
+                        provided.draggableProps.style
+                      )}
+                    >
+                      <Todo
+                        todo={todo}
+                        todoList={todoList}
+                        setTodoList={(todoList) => setTodoList(todoList)}
+                      />
+                    </div>
+                  )}
+                </Draggable>
+              );
+            })}
+            {provided.placeholder}
+          </div>
+        )}
+      </Droppable>
+    </DragDropContext>
   );
 }
